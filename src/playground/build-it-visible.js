da7e551804abd779c4b91ca80049d6205cb40796
David Rytell <daverytell@gmail.com>
2018-05-28 12:42:39 -0700

    End of Lecture 21 - visibility app

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,39 @@
+console.log("Here I am!");
+
+
+const appRoot = document.getElementById("app");
+
+let visibility = false;
+
+const changeIt = () => {
+   
+    visibility = !visibility;
+    
+    // if (visibility == true) {
+    //     visibility = false;
+    // } else
+    // {
+    //     visibility = true;
+    // }
+    renderMe();
+}
+
+
+const renderMe = () => {
+    //JSX - JavaScript XML - a JavaScript syntax "extension" provided by REACT
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={changeIt}>{visibility ? "Hide details" : "Show details"}</button>
+            <p>{visibility && (<p>Here's the hidden content!!</p>)}</p>          
+        </div>
+    );
+    
+    ReactDOM.render(template, appRoot);
+};
+
+renderMe();
+
+
+
+
