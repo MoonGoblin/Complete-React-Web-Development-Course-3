5b008d7d8424491d06b2d37a589de8dc0ab2ac9c
David Rytell <daverytell@gmail.com>
2018-05-27 18:26:31 -0700

    Finished lecture 20 and basic logic of indecision app

--- src/app.js
+++ src/app.js
@@ -23,6 +23,13 @@ const makeZero = () => {
     renderMe();
 };
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    //console.log(randomNum);
+    alert(option);
+};
+
 const appRoot = document.getElementById("app");
 
 const renderMe = () => {
@@ -32,7 +39,7 @@ const renderMe = () => {
             <h1>{app.title}</h1>
             {app.subTitle && <p>{app.subTitle}</p>}
             <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should i do?</button>            
             <button onClick={makeZero}>Remove All</button>
             <ol>
                 {/* map over app.options getting back an array of list items 
