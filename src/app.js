981989448eb07c8d3c1168c3810d822a36f89b34
David Rytell <daverytell@gmail.com>
2018-06-02 20:24:56 -0700

    First look at components before challenge - Lecture 26

--- src/app.js
+++ src/app.js
@@ -1,66 +1,29 @@
-console.log("App.js is running!");
-
-const app = {
-    title: "Indecision App",
-    subTitle: "Put your life in your computer's hands!",
-    options: []
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Indecision</h1>
+            <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
 }
 
-const onFormSubmit = (e) => {
-    e.preventDefault(); //stops full page refresh to just handle the event
-    
-    const option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = "";
-        renderMe();
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
     }
-};
+}
 
-const makeZero = () => {
-    app.options = [];
-    renderMe();
-};
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+    </div>
+);
 
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    //console.log(randomNum);
-    alert(option);
-};
-
-const appRoot = document.getElementById("app");
-
-const renderMe = () => {
-//JSX - JavaScript XML - a JavaScript syntax "extension" provided by REACT
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subTitle && <p>{app.subTitle}</p>}
-            <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should i do?</button>            
-            <button onClick={makeZero}>Remove All</button>
-            <ol>
-                {/* map over app.options getting back an array of list items 
-                (set key and text for each)*/
-            
-                    app.options.map((option) => {
-                        return <li key={option}>{option}</li>
-                    })
-
-                    //Alternate syntax:
-                    //app.options.map((option) => <li key={option}>{option}</li>)
-            }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
-        </div>
-    );
-
-    ReactDOM.render(template, appRoot);
-} ;
-
-renderMe();
+ReactDOM.render(jsx, document.getElementById("app"));
---
+++ src/playground/jsx-indecision.js
@@ -0,0 +1,66 @@
+console.log("App.js is running!");
+
+const app = {
+    title: "Indecision App",
+    subTitle: "Put your life in your computer's hands!",
+    options: []
+}
+
+const onFormSubmit = (e) => {
+    e.preventDefault(); //stops full page refresh to just handle the event
+    
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        renderMe();
+    }
+};
+
+const makeZero = () => {
+    app.options = [];
+    renderMe();
+};
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    //console.log(randomNum);
+    alert(option);
+};
+
+const appRoot = document.getElementById("app");
+
+const renderMe = () => {
+//JSX - JavaScript XML - a JavaScript syntax "extension" provided by REACT
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subTitle && <p>{app.subTitle}</p>}
+            <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should i do?</button>            
+            <button onClick={makeZero}>Remove All</button>
+            <ol>
+                {/* map over app.options getting back an array of list items 
+                (set key and text for each)*/
+            
+                    app.options.map((option) => {
+                        return <li key={option}>{option}</li>
+                    })
+
+                    //Alternate syntax:
+                    //app.options.map((option) => <li key={option}>{option}</li>)
+            }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+} ;
+
+renderMe();
