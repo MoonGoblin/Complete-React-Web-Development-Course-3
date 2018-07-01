01fdf3ca0e692896a338704602c3d1db74982e61
David Rytell <daverytell@gmail.com>
2018-07-01 12:54:42 -0700

    Lecture 52 - default export

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 //import "./utils.js";
-import { square, add } from "./utils.js"; // Not object but import syntax
+import subtract, { square, add } from "./utils.js"; // Not object but import syntax
 import { isAdult, canDrink } from "./person.js";
 
 console.log("app.js is running!!!!!");
 
 console.log(square(4));
 console.log(add(100, 23));
+console.log(subtract(100, 75));
 console.log("Is adult? " + isAdult(19));
 console.log("Can drink? " + canDrink(19));
--- src/utils.js
+++ src/utils.js
@@ -4,13 +4,15 @@ const square = (x) => x * x;
 
 const add = (a, b) => a + b;
 
+const subtract = (a, b) => a - b;
+
 // two types of exports
 // exports - default export - named exports
 
 // Let's explore named exports
 // Curly braces not an object here - but things we want to export
 
-export { square, add };
+export { square, add, subtract as default };
 
 // Could also do it this way:
 // export const square = (x) => x * x;
