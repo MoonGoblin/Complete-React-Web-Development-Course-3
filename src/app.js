3cbb829c8ead12330f94df79572d160c72a96fd3
David Rytell <daverytell@gmail.com>
2018-07-07 08:48:50 -0700

    Added babel plugin for new syntax - will help remove need for constructor functions

--- src/app.js
+++ src/app.js
@@ -8,3 +8,27 @@ import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+
+class OldSyntax {
+    constructor() {
+        this.name = "Mike";
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+// ------------
+// New syntax with babel plug-in eliminates the 
+// need for making a cumbersome constructor function
+// however, in our first iteration we haven't yet eliviated the
+// need for binding in the constructor function in IndecisionApp
+// so we won't be able to completely take the constructor function
+// away there yet.
+
+class NewSyntax {
+    name = "Jen";
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
