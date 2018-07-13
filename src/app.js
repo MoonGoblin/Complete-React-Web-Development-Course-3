da0f114d67e5ff1362a59008f45219ddb1f56967
David Rytell <daverytell@gmail.com>
2018-07-12 19:50:56 -0700

    Lecture 65 - beginning styling with scss

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-// Challenge
-// Moving the component code
-// Setup import/export
-// Move the imports to that new file (and setup an import here)
-
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
